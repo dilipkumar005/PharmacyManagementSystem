90a1dbb63c2e0f3cc9271329837876d102fa747c
Dilip Kumar <dilipkumar8923@gmail.com>
2025-05-31 23:12:08 +0530

    Update Table.java

--- src/dao/Table.java
+++ src/dao/Table.java
@@ -19,7 +19,7 @@ public class Table {
         st.executeUpdate("Create table appuser(appuser_pk int AUTO_INCREMENT primary key,userRole varchar(200),name varchar(200),dob varchar(50),mobileNumber varchar(50),email varchar(200),username varchar(200),password varchar(50),address varchar(200))");
         st.executeUpdate("insert into appuser (userRole,name,dob,mobileNumber,email,username,password,address) values ('Admin','Admin','16-12-2001','0123456789','admin@email.com','admin','admin','India')");
         st.executeUpdate("create table medicine(medicine_pk int AUTO_INCREMENT primary key, uniqueId varchar(200),name varchar(200),companyName varchar(200),quantity bigint,price bigint)");
-          st.executeUpdate("create table bill(bill_pk int AUTO_INCREMENT primary key, billId varchar(200), billDate varchar(50), totalPaid bigint, generatedBy varchar(50))");
+          st.executeUpdate("create table bill(bill_pk int AUTO_INCREMENT primary key, billId varchar(200), billDate varchar(50), totalPaid bigint, generatedBy varchar(50),userRole varchar(200))");
         JOptionPane.showMessageDialog(null, "Table Created Successfully");
                 }
         catch(Exception e){
